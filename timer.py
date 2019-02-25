d326c068f6fa253d22ff840cad3a2f5ccdde7180
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2019-02-25 17:46:06 +0000

    Add timer

---
+++ timer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import subprocess
+import re
+
+RUNS = 5
+
+p = re.compile("[0-9]:(([0-9]*[.])?[0-9]+)elapsed")
+
+times = []
+for i in range(RUNS):
+  process = subprocess.Popen("time ./kdtreetest 5000000", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  out, err = process.communicate()
+  parsed = p.search(err.decode())
+  t = float(parsed.group(1))
+  print("Run", i, ":", t)
+  times.append(t)
+
+total = 0
+for t in times:
+  total += t
+
+print("Average time: ", total / RUNS)
